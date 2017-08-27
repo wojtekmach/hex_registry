3fbc8b652407305a2a5d8cdb0a97720fbd9c926b
Wojtek Mach <wojtek@wojtekmach.pl>
2017-08-28 00:03:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+hex_registry
+=====
+
+An OTP library
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ test/hex_registry_tests.erl
@@ -0,0 +1,5 @@
+-module(hex_registry_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+sanity_test() ->
+    ok.
